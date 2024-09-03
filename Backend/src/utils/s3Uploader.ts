9a00f857f99ec86498a010331ad8a37c17309d00
ashnavs <ashnavs15@gmail.com>
2024-09-03 17:42:00 +0530

    blah blah

--- Backend/src/utils/s3Uploader.ts
+++ Backend/src/utils/s3Uploader.ts
@@ -1,7 +1,7 @@
+import dotenv from 'dotenv';
 dotenv.config();
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 import { v4 as uuidv4 } from 'uuid';
-import dotenv from 'dotenv';
 
 
 
